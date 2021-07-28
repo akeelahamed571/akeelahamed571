8dd87e5d16f7aeb894d9d7f00e1aa6f8672b716d
akeel <it20068264@my.sliit.lk>
2021-07-28 09:38:42 +0530

    Edureka case

---
+++ src/main/java/com/edureka/demo/EdurekaApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurekaApplication.class, args);
+	}
+
+}
